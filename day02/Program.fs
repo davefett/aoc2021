8a4e2f276ec7100be6a72c3b6615bcd74fbb1374
David Fetterman <dave@ebrdesign.com>
2021-12-02 12:35:18 -0500

    day 2 complete

--- day02/Program.fs
+++ day02/Program.fs
@@ -1,4 +1,48 @@
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main argv =
+
+    let matchCommand input =
+        let m =
+            Regex.Match(input, @"(forward|up|down)\s+(\d+)")
+        // matching count to a set value feels wrong, is there a more f#'y way to do this?
+        match m.Groups.Count with
+        | 3 -> Some(m.Groups.[1] |> string, m.Groups.[2].Value |> int)
+        | _ -> None
+
+    let commands =
+        let path = Path.Combine(".", "input02.txt")
+
+        File.ReadAllLines path
+        |> Array.choose matchCommand
+        |> List.ofArray
 
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+    let rec runCommands cmds horiz vert =
+        match cmds with
+        | [] -> printfn $"horizontal: %d{horiz} * %d{vert} = %d{horiz * vert}"
+        | (direction: string, dist: int) :: tail ->
+            match direction with
+            | "forward" -> runCommands tail (horiz + dist) vert
+            | "up" -> runCommands tail horiz (vert - dist)
+            | "down" -> runCommands tail horiz (vert + dist)
+            | _ -> printfn "error"
+
+    runCommands commands 0 0
+
+    let rec runCommandsWithAim cmds aim horiz vert =
+        match cmds with
+        | [] -> printfn $"horizontal: %d{horiz} * %d{vert} = %d{horiz * vert}"
+        | (direction: string, dist: int) :: tail ->
+            match direction with
+            | "forward" -> runCommandsWithAim tail aim (horiz + dist) (vert + (aim * dist))
+            | "up" -> runCommandsWithAim tail (aim - dist) horiz vert
+            | "down" -> runCommandsWithAim tail (aim + dist) horiz vert
+            | _ -> printfn "error"
+
+    runCommandsWithAim commands 0 0 0
+
+    0
